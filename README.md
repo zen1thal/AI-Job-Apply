ef22c99ee90f1553afda7471939de899860feccb
zenthal <sviatoslav.syniuk2003@gmail.com>
2025-11-12 16:06:26 +0100

    Update login and apply with easy button (current Chrome Profile)

--- README.md
+++ README.md
@@ -8,11 +8,39 @@ The code development is still in progress and it's made only for research reason
 AI functionality model to respond custom questions that are outside of the algorithm. Any contributions would pretty
 appreciated since the idea of this project consists of learning Python libraries while working with AI models.
 
+## Requirements
+
+- Install the same version of Chrome and ChromeDriver. Watch compatibilities here: https://developer.chrome.com/docs/chromedriver/downloads
+- Python version 3.9+
+
 ## Setup
+
+Update the config.json:
+
+```json
+username: # Insert your username here
+password: # Insert your password here
+
+keywords:
+- # positions you want to search for
+- # Another position you want to search for
+- # A third position you want to search for
+
+location:
+- # Location you want to search for
+
+Chrome User Directory:
+- # Example MacOS: /Users/name_of_the_user/Library/Application Support/Google/Chrome
+
+Profile:
+- # Current Chrome Profile
+```
+
 
 Used Python 3.9.4 on MacOS via Virtual Environment/Conda
 
 Before running, install the necessary libraries
+
 ```bash
 pip3 install -r requirements.txt
 ```
--- main.py
+++ main.py
@@ -1,57 +1,46 @@
+import json
 import time
 import random
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 # from selenium.webdriver.firefox.options import Options
+# from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 KEYWORDS = ["junior developer", "fullstack", "backend", "frontend"]
 
-# FIREFOX_PROFILE_PATH = "/Users/zviatos/Library/Application Support/Firefox/Profiles/l0tx736r.default-release"
-CHROME_USER_DATA_DIR = "/Users/zviatos/Library/Application Support/Google/Chrome"
-CHROME_PROFILE = "Profile 1"  
-
-def main():
-    username = ""
-    password = ""
-    opts = browser_setup()
-    service = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome(service=service, options=opts)
-    # TODO: Add the functionality to load with existing profile data without crashing
-    login(browser, username, password)
-    start_apply(browser)
+with open('config.json', 'r') as file:
+    config = json.load(file)
 
 def human_delay(min_seconds=1, max_seconds=5):
     time.sleep(random.uniform(min_seconds, max_seconds))
 
 def browser_setup():
-    # -------- Configure Firefox Profile --------
-    # opts = Options()
-    # opts.profile = FIREFOX_PROFILE_PATH
-
-
     # -------- Configure Chrome Profile --------
     opts = webdriver.ChromeOptions()
-
-    # opts.add_argument(f"--user-data-dir={CHROME_USER_DATA_DIR}")
-    # opts.add_argument(f"--profile-directory={CHROME_PROFILE}")
+    opts.add_argument(rf"--user-data-dir={config['chrome_user_data_dir']}")
+    opts.add_argument(rf"--profile-directory={config['chrome_profile']}")
 
     opts.add_argument("--start-maximized")
-    opts.add_argument("--disable-blink-features=AutomationControlled")
     opts.add_argument("--disable-dev-shm-usage")
+    opts.add_argument("--ignore-certificate-errors")
     opts.add_argument("--no-sandbox")
-    # Can be useful on Mac to disable GPU
+    # # Can be useful on Mac to disable GPU
     opts.add_argument("--disable-gpu")
-    opts.add_argument("--remote-debugging-port=9222")
     opts.add_experimental_option("excludeSwitches", ["enable-automation"]) 
     opts.add_experimental_option('useAutomationExtension', False)
+    opts.add_argument("--remote-debugging-port=9222")
 
+    # browser = webdriver.Firefox(options=opts)
+    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=opts)
 
     print("Chrome options configured.")
-    return opts
+    return browser
 
 def login(browser, username,password):
     browser.get("https://www.linkedin.com/login")
@@ -68,14 +57,15 @@ def login(browser, username,password):
 def start_apply(browser):
 
     try:
-        for keyword in KEYWORDS:
+        for keyword in config['keywords']:
             print(f"Searching for jobs with keyword: {keyword}")
             
-            browser.get(f"https://www.linkedin.com/jobs/search/?keywords={keyword.replace(' ', '%20')}&location=Madrid%2C%20Spain") 
+            browser.get(f"https://www.linkedin.com/jobs/search/?keywords={keyword.replace(' ', '%20')}f_AL=true&location={config['location']}&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0") 
             human_delay(3, 7)
             # TODO: Finish the logic of job application
+            
             # Lisf of jobs
-            links = browser.find_element("xpath", "//div[@data-job-id]")
+            links = browser.find_elements(By.XPATH, "//div[@data-job-id]")
             print(f"Processing job listing: {links}")
 
             for job in links:
@@ -89,8 +79,7 @@ def start_apply(browser):
                     # human_delay(2, 5)
 
                     # "Easy Apply" button
-                    easy_apply_button = browser.find_element("xpath",'//button[contains(@class, "jobs-apply-button")]')
-                    easy_apply_button.click()
+                    browser.find_element(By.ID,'jobs-apply-button-id').click()
                     human_delay(2, 4)
                     
                     # Click "Next" if it exists
@@ -113,7 +102,7 @@ def start_apply(browser):
                     print("Application submitted (simulation).")
 
 
-                    # For now, just close the application modal to continue to the next job
+                    # Close the application modal
                     close_button = browser.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
                     close_button.click()
                     human_delay(1, 2)
@@ -131,10 +120,16 @@ def start_apply(browser):
         print(f"Could not complete the search for keyword '{keyword}': {e}")
 
     # TODO: AI integration for cover letter generation or custome responses
-    
     # Close browser after some time
     browser.quit()
 
+def main():
+    browser = browser_setup()
+    print("Browser opened with profile")
+     
+    login(browser, config['username'], config['password'])
+    start_apply(browser)
+
 
 if __name__ == "__main__":
-    main()
+   main()
